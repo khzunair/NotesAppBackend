2f200540601aa69c550f751043c8c3829b14ce2f
Zunair Khawaja <102663927+khzunair@users.noreply.github.com>
2022-10-26 00:14:06 +0500

    added delete and update feature

--- src/main/java/com/eagleLearns/springboot/controller/NotesController.java
+++ src/main/java/com/eagleLearns/springboot/controller/NotesController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,10 @@ public class NotesController {
 		public ResponseEntity<HttpStatus> deleteNote(@PathVariable Long id){
 		nRepo.deleteById(id);
 		return new  ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT); 
+	}
+	@PutMapping("/notes")
+	public ResponseEntity<Notes> updateNote(@RequestBody Notes note){
+		return new  ResponseEntity<Notes>(nRepo.save(note),HttpStatus.OK);
 	}
 		
 		
